443ac1f35773e01258c464d0ef7643554d172ac5
shawtymarco <games08yt@gmail.com>
2026-03-25 23:46:15 +0900

    fix: remove final from ItemBlock to allow plugin subclassing

--- src/item/ItemBlock.php
+++ src/item/ItemBlock.php
@@ -33,7 +33,7 @@ use pocketmine\data\runtime\RuntimeDataDescriber;
  * This should NOT be used for items which are merely *associated* with blocks (e.g. seeds are not wheat crops; they
  * just place wheat crops when used on the ground).
  */
-final class ItemBlock extends Item{
+class ItemBlock extends Item{
 	public function __construct(
 		public Block $block
 	){
